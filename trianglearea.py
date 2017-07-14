effbe273b79664186a031f2cf7beb8a85ae407fe
DhanyaMohandas <dhanyapisharath92@gmail.com>
2017-07-14 15:44:49 +0530

    Add files via upload

---
+++ trianglearea.py
@@ -0,0 +1,3 @@
+b=input('Base of the triangle=')
+h=input('Height of the triangle=')
+print 'Area of the triangle=' , 0.5 * b * h
---
+++ within.py
@@ -0,0 +1,6 @@
+x=input('Enter a number=')
+if (x>1000) & (x<2000):
+    print 'Between 1000 and 2000' 
+else:
+    print 'Not between 1000 and 2000'
+
